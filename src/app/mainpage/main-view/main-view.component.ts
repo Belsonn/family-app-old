f527e84e7ae919afd2821822a92b4e9224f833fc
Mateusz Belka <mateusz.belka14@gmail.com>
2020-05-09 02:41:55 +0200

    Refactor Main-View

--- src/app/mainpage/main-view/main-view.component.ts
+++ src/app/mainpage/main-view/main-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/authentication/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-view',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-view.component.css']
 })
 export class MainViewComponent implements OnInit {
+  isLoading = false;
+  hasFamily = false;
+  isAuthenticated = false;
+  private authStatus: Subscription 
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.isLoading = true;
+    this.isAuthenticated = this.authService.getIsAuthenticated();
+    this.hasFamily = this.authService.getHasFamily();
+    this.authStatus = this.authService.getAuthStatus().subscribe(isAuthenticated =>{
+      this.isLoading = false;
+      this.isAuthenticated = isAuthenticated;
+    })
+    if(!this.isAuthenticated) {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  onTest() {
+    this.router.navigate(["home", "test"]);
+  }
+  
+  ngOnDestroy() {
+    this.authStatus.unsubscribe();
   }
 
 }
