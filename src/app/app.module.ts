329e30aa40e8c36a30452d9ff8c39f498164ca4a
Mateusz Belka <mateusz.belka14@gmail.com>
2020-05-09 02:42:34 +0200

    more imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,38 +9,54 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './authentication/signup/signup.component';
-import { HeaderComponent } from './header/header.component';
-import { MainPageComponent } from './mainpage/mainpage.component';
 import { MainViewComponent } from './mainpage/main-view/main-view.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoginComponent } from './authentication/login/login.component';
 import { AuthInterceptor } from './authentication/auth.interceptor';
+import { HomepageComponent } from './mainpage/homepage/homepage.component';
+import { FamilyViewComponent } from './mainpage/familyView/familyView.component';
+import { MenuComponent } from './mainpage/menu/menu.component';
+import { SmallheaderComponent } from './mainpage/smallheader/smallheader.component';
+import { FamilyCreateComponent } from './mainpage/family-create/family-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    HeaderComponent,
-    MainPageComponent,
     MainViewComponent,
     LoginComponent,
+    HomepageComponent,
+    FamilyViewComponent,
+    MenuComponent,
+    SmallheaderComponent,
+    FamilyCreateComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule,
-    MatButtonModule,
     BrowserAnimationsModule,
     FormsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
     MatFormFieldModule,
     MatProgressSpinnerModule,
     MatInputModule,
     MatExpansionModule,
     MatCardModule,
-    HttpClientModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatListModule,
+    MatMenuModule,
+    MatRadioModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
